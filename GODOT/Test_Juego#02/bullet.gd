25c8f8686e9a455a330e6def9832fcde5d5b82e0
Ian Rios <ian.gabrielrc@gmail.com>
2026-09-24 21:16:05 -0300

    Add files via upload

---
+++ GODOT/Test_Juego#02/bullet.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+const SPEED: int = 300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.x * SPEED * delta
